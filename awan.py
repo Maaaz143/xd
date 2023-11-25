ea67089170f0aade7be2aabda4b0f10e8972f5bc
AWAN-XD <145342995+AWAN-143@users.noreply.github.com>
2023-11-25 18:14:51 +0500

    Update awan.py

--- awan.py
+++ awan.py
@@ -3,4 +3,4 @@ import time
 os.system("git pull")
 print('lodinglodig....!');time.sleep(1.3)
 print('tool off \n contact with owner')
-os.system('xdg-open https://wa.me/0334912454');exit()
+os.system('xdg-open https://wa.me/+92334912454');exit()
