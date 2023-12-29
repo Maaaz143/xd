44b1bf419f7288916113ec5663d42c3371c6b65f
AWAN-XD <145342995+AWAN-143@users.noreply.github.com>
2023-12-29 12:43:17 +0500

    Update awan.py

--- awan.py
+++ awan.py
@@ -1,6 +1,4 @@
 import os
 os.system('git pull')
 print('loding....')
-print(" tool off for some time for any error contact with owner")
-os.system("xdg-open https://wa.me/+923334912454")
-exit()
+import yz
